4f2079ccc24ff94f7376918d6506e2c7d977872a
Ben Stone <benjamin.h.stone@gmail.com>
2020-01-02 17:18:46 -0500

    remove files

--- DrawingWithBezierPaths/LineDrawingViewController.swift
+++ DrawingWithBezierPaths/LineDrawingViewController.swift
@@ -1,12 +1,7 @@
 import UIKit
 
-class LineDrawingViewController: UIViewController {
-    @IBOutlet var lineView: LineView! {
-        didSet {
-            let handler = #selector(LineView.changeControlPoint(byReactingTo:))
-            let tapRecognizer = UITapGestureRecognizer(target: lineView, action: handler)
-            tapRecognizer.numberOfTapsRequired = 1
-            lineView.addGestureRecognizer(tapRecognizer)
-        }
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 }
